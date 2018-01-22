c2c4295c2598b2af6435e188a730fdc1307348fd
Ben French <ben.jminw@gmail.com>
2018-01-21 17:53:36 -0700

    Loading modules and committing initial package.json.

--- liri.js
+++ liri.js
@@ -1,13 +1,18 @@
+// Load API keys from file
 keys = require('./keys.js');
 
-// var Twitter = require('twitter');
-// var Spotify = require('node-spotify-api');
+// Load Twitter, Spotify, and Requests modules
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
 
+// Initialize Spotify client
 var spotifyClient = new Spotify({
     id: keys.spotifyKeys.client_id,
     secret: keys.spotifyKeys.client_secret
 });
 
+// Initialize Twitter client
 var twitterClient = new Twitter({
     consumer_key: keys.twitterKeys.consumer_key,
     consumer_secret: keys.twitterKeys.consumer_secret,
