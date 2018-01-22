f161893af2f1fc478d59593ed4c2a9d7b675f298
Ben French <ben.jminw@gmail.com>
2018-01-21 17:50:17 -0700

    Adding keys and initialization for Spotify and Twitter node clients.

--- keys.js
+++ keys.js
@@ -1,10 +1,19 @@
-console.log('this is loaded');
-
 var twitterKeys = {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: '',
+    consumer_key: '0xQ97Sdo02nhOG0dtK9yw22Ex',
+    consumer_secret: 'Y31TXHFSxCsJzXhORwFS51YHqthvoENYXpdWs8Nd2pgzeixO4f',
+    access_token_key: '955236608709554176-1kJ42fi8U9VGo1FmhvMIYBYLJwKOcsE',
+    access_token_secret: 'Obm4udNuW1FbxpECzIjnSNqWQssQA2ETqISDz9PZvw44D',
 }
 
-module.exports = twitterKeys;
+var spotifyKeys = {
+    client_id: 'f98cd7abdcbb48f1a6859134f996ff7f',
+    client_secret: '4cede86b00644462a087626306daa123'
+}
+
+var omdbKeys = {
+    api_key: '966444d6'
+}
+
+module.exports.twitterKeys = twitterKeys;
+module.exports.spotifyKeys = spotifyKeys;
+module.exports.omdbKeys = omdbKeys;
--- liri.js
+++ liri.js
@@ -1,1 +1,16 @@
 keys = require('./keys.js');
+
+// var Twitter = require('twitter');
+// var Spotify = require('node-spotify-api');
+
+var spotifyClient = new Spotify({
+    id: keys.spotifyKeys.client_id,
+    secret: keys.spotifyKeys.client_secret
+});
+
+var twitterClient = new Twitter({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
+});
